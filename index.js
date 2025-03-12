df53f194c9a791843a193fffd5c5ccd068cb0b08
Spencer Chin <126372872+SpencerrrC@users.noreply.github.com>
2025-03-12 16:58:05 -0400

    Create CPU Algorithm

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+console.log("hello world. Let's get this game started")
+
+// Computer's decision
+function getComputerChoice(computerResponse) {
+    
+    computerResponse = Math.random();
+
+    if (computerResponse <= .33) { 
+        return console.log("Rock");
+    }
+
+    else if (computerResponse <= .66) { 
+        return console.log("Paper");
+    }
+
+    else {
+        return console.log("Scissors");
+    }
+    
+};
+
+getComputerChoice(1);
+
+
